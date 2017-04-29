071b9da6f841e7875db9041b6526557070c1c5c3
rtaimal <rtaimal@gmail.com>
2017-04-29 17:43:06 -0500

    initial commit

--- app.js
+++ app.js
@@ -113,7 +113,5 @@ else {
         //__dirname : It will resolve to your project folder.
     });
 
-    app.listen(3000, function () {
-        console.log('Listening on port 3001...')
-    });
+    app.listen(process.env.PORT || 3000);
 }
